5c579cd80c7d3edec83e70dd563edd2712ddc700
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-06-25 22:38:04 +0200

    added header and footer

--- Frontend/app/pages/_app.tsx
+++ Frontend/app/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Footer from '../components/Footer'
+import { Header } from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider> 
+  )
 }
 
 export default MyApp
