dc26d18e857e9306f1318dde33d0117cba810bf9
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-15 22:04:43 +0200

    added download cv functionality

---
+++ Frontend/app/components/header/CTA.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href="cv.pdf" download>
+        Download CV
+      </a>
+      <a href="#contact">Contact me</a>
+    </div>
+  );
+}
+
+export default CTA
--- Frontend/app/components/header/Header.tsx
+++ Frontend/app/components/header/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './header.module.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Jonatan Schultz</h1>
+        <h5 className="text-light">Fullstack developer</h5>
+        <CTA />  
+      </div>  
+    </header>
   )
 }
 
