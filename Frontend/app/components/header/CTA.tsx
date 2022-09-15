a063e534a7b25622f93b152e9cbb11f1791f265a
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-15 22:14:36 +0200

    startpage image

--- Frontend/app/components/header/CTA.tsx
+++ Frontend/app/components/header/CTA.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="cv.pdf" download>
+      <a href="cv.pdf" download className="btn">
         Download CV
       </a>
-      <a href="#contact">Contact me</a>
+      <a href="#contact" className='btn btn-primary'>Contact me</a>
     </div>
   );
 }
--- Frontend/app/components/header/Header.tsx
+++ Frontend/app/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './header.module.css'
 import CTA from './CTA'
+import Image from 'next/image';
 
 const Header = () => {
   return (
@@ -9,7 +10,11 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Jonatan Schultz</h1>
         <h5 className="text-light">Fullstack developer</h5>
-        <CTA />  
+        <CTA />
+
+        <div className="me">
+          <Image src="/man.jpg" alt="" layout="responsive" width="513" height="640"/>  
+        </div>  
       </div>  
     </header>
   )
