90e0e25943be75ac1d8c079762283039e8b969ea
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-16 21:08:23 +0200

    Experience

--- Frontend/app/components/experience/Experience.tsx
+++ Frontend/app/components/experience/Experience.tsx
@@ -1,17 +1,163 @@
 import React from 'react'
+import { BsPatchCheckFill } from 'react-icons/bs';
+
+
 
 
 const Experience = () => {
+
   return (
     <section id="experience">
       <h5>My Skills</h5>
       <h2>My Experience</h2>
 
       <div className="container experience__container">
-        
+        <div className="experience__dev experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>TypeScript</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>GIT</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>DevOps</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>AGILE</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Project Management</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__dev experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>.NET</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>SQL</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>NodeJS</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>REST</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+      <div className="essential__experience">
+        <div className="experience__essential">
+          <h3>Essential Skills</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>GIT</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>DevOps</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>AGILE</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Project Management</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Quality Assurance</h4>
+                <small className="text-light">Proficient</small>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default Experience
