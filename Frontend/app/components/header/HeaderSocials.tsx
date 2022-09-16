9e5757f27197473dfb85301f4600817a5e2bb5ee
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-16 23:11:31 +0200

    correct linkedin url

--- Frontend/app/components/header/HeaderSocials.tsx
+++ Frontend/app/components/header/HeaderSocials.tsx
@@ -7,7 +7,7 @@ import { FiDribbble } from "react-icons/fi"
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://linkedin.com/jonatan_schultz" target="blank">
+      <a href="https://linkedin.com/in/jonatan-schultz/" target="blank">
         <BsLinkedin/>
       </a>
       <a href="https://github.com/exemplarisk" target="blank">
