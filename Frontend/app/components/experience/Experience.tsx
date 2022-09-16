1f2da2598229621d807974f42955418779b58b58
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-16 18:52:37 +0200

    styling changes

--- Frontend/app/components/experience/Experience.tsx
+++ Frontend/app/components/experience/Experience.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>My Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        
+      </div>
+    </section>
   )
 }
 
