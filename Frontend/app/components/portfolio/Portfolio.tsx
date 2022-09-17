33fa050299d9ee61c9aec675c5a28ed4bccc7eae
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-17 09:33:59 +0200

    services

--- Frontend/app/components/portfolio/Portfolio.tsx
+++ Frontend/app/components/portfolio/Portfolio.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
-  )
+    <section id="portfolio">
+      <h5>What I've done</h5>
+      <h2>Portfolio</h2>
+
+      <div className="">
+        <article className="">
+          <div className="">
+            <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit cum pariatur ea quos, provident obcaecati rerum soluta ab? Animi, ex.</h3>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio
--- Frontend/app/components/services/Services.tsx
+++ Frontend/app/components/services/Services.tsx
@@ -1,9 +1,143 @@
 import React from 'react'
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
-  )
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>System/Web development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>Web development using modern frameworks</p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>Release and deploy</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Agile transformation and Quality Assurance</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Project Management</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, esse?
+              </p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Services
