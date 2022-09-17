ff74aacf28441720acfc0aa9d857567e78b4a46d
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-17 22:04:08 +0200

    footer created

--- Frontend/app/components/footer/Footer.tsx
+++ Frontend/app/components/footer/Footer.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <section id="footer">Footer</section>
-  )
+    <footer>
+      <a href="#" className="footer__logo">
+        Jonatan Schultz
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Recent Work</a>
+        </li>
+        <li>
+          <a href="#story">About Me</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://linkedin.com/in/jonatan-schultz/" target="blank">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/exemplarisk">
+          <FaGithub />
+        </a>
+        <a href="/cv.pdf">CV</a>
+      </div>
+
+      <div className="footer__copyright">
+      <small>&copy; Jonatan Schultz</small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
--- Frontend/app/components/services/Services.tsx
+++ Frontend/app/components/services/Services.tsx
@@ -14,31 +14,31 @@ const Services = () => {
           </div>
           <ul className="service__list">
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Responsive Web development</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Mobile Applications</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>E-Commerce Development</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Content Management System Setup</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Databases and APIs</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Cloud Computing</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Release and deployment</p>
             </li>
           </ul>
@@ -49,31 +49,31 @@ const Services = () => {
           </div>
           <ul className="service__list">
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Agile Transformation</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Scrum/Kanban Transition</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Communications Strategy</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Culture evolution strategies</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Team Building</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Vision Setting</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Security First</p>
             </li>
           </ul>
@@ -84,31 +84,31 @@ const Services = () => {
           </div>
           <ul className="service__list">
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Agile Project Management</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Resource Management</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Estimating</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Planning and Scheduling</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Document Control</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>JIRA/DEVOPS</p>
             </li>
             <li>
-              <BiCheck className="service__list_icon" />
+              <BiCheck className="service__list-icon" />
               <p>Quality Assurance</p>
             </li>
           </ul>
