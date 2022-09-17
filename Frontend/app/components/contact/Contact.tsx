09239840823968d3d814661bf1168cc41aa00ffe
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-17 20:45:29 +0200

    contact me section started

--- Frontend/app/components/contact/Contact.tsx
+++ Frontend/app/components/contact/Contact.tsx
@@ -1,10 +1,64 @@
 import React from 'react'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import { BsLinkedin } from "react-icons/bs";
+
 
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
-  )
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <BsLinkedin className="contact__option-icon" />
+            <h4>Linkedin</h4>
+            <h5>Jonatan Schultz</h5>
+            <a href="https://linkedin.com/in/jonatan-schultz/" target="blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Jonatan Schultz</h5>
+            <a href="https://m.me/jonatan.schultz.1" target="blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>jonatan_schultz@hotmail.com</h5>
+            <a href="mailto:jonatan_schultz@hotmail.com" target="blank">
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows={7}
+            placeholder="Your Message"
+            required
+          />
+          <button className="btn btn-primary" type="submit">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact
