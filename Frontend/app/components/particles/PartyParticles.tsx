71969c7b80979b961acb585eb322ec0790432a2e
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-09-30 21:11:18 +0200

    changes to particles

--- Frontend/app/components/particles/PartyParticles.tsx
+++ Frontend/app/components/particles/PartyParticles.tsx
@@ -65,7 +65,7 @@ const PartyParticles = () => {
             direction: "none",
             enable: true,
             outModes: {
-              default: "bounce",
+              default: "destroy",
             },
             random: false,
             speed: 3,
