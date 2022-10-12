9c4c763cb5da5e0cfef278ea37d0008ffaac89d2
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-10-12 22:06:05 +0200

    mapping out links in footer

--- Frontend/app/components/footer/Footer.tsx
+++ Frontend/app/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
+import { links } from "./data/links";
 
 const Footer = () => {
   return (
@@ -8,44 +9,23 @@ const Footer = () => {
       <a href="#" className="footer__logo">
         Jonatan Schultz
       </a>
-
       <ul className="permalinks">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#portfolio">Recent Work</a>
-        </li>
-        <li>
-          <a href="#story">About Me</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
+        {links.map((x) => {
+          return (
+            <li key={x.name}>
+              <a href={x.href}>{x.name}</a>
+            </li>
+          );
+        })}
       </ul>
-
       <div className="footer__socials">
-        <a
-          href="https://linkedin.com/in/jonatan-schultz/"
-          target="blank"
-        >
+        <a href="https://linkedin.com/in/jonatan-schultz/" target="blank">
           <BsLinkedin />
         </a>
         <a href="https://github.com/exemplarisk">
           <FaGithub />
         </a>
-        <a href="/cv.pdf">
-          CV
-        </a>
+        <a href="/cv.pdf">CV</a>
       </div>
 
       <div className="footer__copyright">
@@ -53,6 +33,6 @@ const Footer = () => {
       </div>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
---
+++ Frontend/app/components/footer/data/links.js
@@ -0,0 +1,30 @@
+export const links = [
+  {
+    name: "Home",
+    href: "#",
+  },
+  {
+    name: "About",
+    href: "#about",
+  },
+  {
+    name: "Experience",
+    href: "#experience",
+  },
+  {
+    name: "Services",
+    href: "#services",
+  },
+  {
+    name: "Portfolio",
+    href: "#portfolio",
+  },
+  {
+    name: "Story",
+    href: "#story",
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+  },
+];
