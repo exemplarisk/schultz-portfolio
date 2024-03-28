8e338abcf76b4d6d471c452ac05cf3ab7f06bf77
Jonatan Schultz <jonatan.schultz@blaklader.com>
2024-03-28 15:28:47 +0100

    Only show dlsavatar on desktop & tablet

--- Frontend/app/components/header/Header.tsx
+++ Frontend/app/components/header/Header.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials';
 import DLSAvatar from '../avatar/DLSAvatar';
+import { useDeviceType, DeviceType } from '../../utils/useDeviceType';
 
 const Header = () => {
-
+  const deviceType = useDeviceType();
   const modelSrc = "https://models.readyplayer.me/65464e4ce42e04abf894cb73.glb";
 
     return (
@@ -17,14 +18,14 @@ const Header = () => {
               Ideas Engineer & Miscellaneous Specialist
             </h5>
             <CTA />
-            <div style={{ pointerEvents: "none" }} className="DLSAvatar">
+            {deviceType !== DeviceType.Mobile && <div style={{ pointerEvents: "none" }} className="DLSAvatar">
               <DLSAvatar
                 modelSrc={modelSrc}
                 animationSrc="/Salute.fbx"
                 cameraInitialDistance={5}
                 cameraTarget={1.0}
               />
-            </div>
+            </div>}
             <HeaderSocials />
             <a href="#contact" className="scroll__down">
               Scroll Down
