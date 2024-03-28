54dab70548404b0468cc92be349d5f20dd772f30
Jonatan Schultz <jonatan.schultz@blaklader.com>
2024-03-28 15:35:47 +0100

    Patch

--- Frontend/app/utils/useDeviceType.ts
+++ Frontend/app/utils/useDeviceType.ts
@@ -1,34 +1,32 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react';
 
 export enum DeviceType {
-    Mobile,
-    Tablet,
-    Desktop,
-  }
-  
-  export const useDeviceType = (): DeviceType => {
-    const isSSR = typeof window === "undefined";
-    
-    const [deviceType, setDeviceType] = useState<DeviceType>(() => {
-      const width = isSSR ? 0 : window.innerWidth;
-      if (width < 768) return DeviceType.Mobile;
-      else if (width >= 768 && width <= 1024) return DeviceType.Tablet;
-      else return DeviceType.Desktop;
-    });
-  
-    React.useEffect(() => {
-      if (isSSR) return;
-  
-      const handleResize = () => {
-        const width = window.innerWidth;
-        if (width < 768) setDeviceType(DeviceType.Mobile);
-        else if (width >= 768 && width <= 1024) setDeviceType(DeviceType.Tablet);
-        else setDeviceType(DeviceType.Desktop);
-      };
-  
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
-  
-    return deviceType;
-  };  
+  Mobile,
+  Tablet,
+  Desktop,
+}
+
+export const useDeviceType = (): DeviceType => {
+
+  const [deviceType, setDeviceType] = useState<DeviceType>(DeviceType.Desktop);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 768) {
+        setDeviceType(DeviceType.Mobile);
+      } else if (width >= 768 && width <= 1024) {
+        setDeviceType(DeviceType.Tablet);
+      } else {
+        setDeviceType(DeviceType.Desktop);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return deviceType;
+};
