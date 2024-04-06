0f3311e603109f098dda94f8a66bedd6a5597133
Jonatan Schultz <jonatan.schultz@blaklader.com>
2024-04-06 10:11:46 +0200

    Update info

--- Frontend/app/components/story/data/storys.js
+++ Frontend/app/components/story/data/storys.js
@@ -8,13 +8,13 @@ export const storys = [
   {
     id: 2,
     title: "Originally from Sweden",
-    text: "But I’ve made homes in Guatemala, Spain, and China. Experiencing different cultures, uncovering their histories, and learning how they shape societies - is what ignites my passion. It is a cornerstone of my life's mission: to understand, and truly appreciate the wide array of global cultures. Drawing inspiration from each one of them, and merging these experiences into the story that defines me.",
+    text: "But I’ve made homes in Guatemala, Spain, and China. Experiencing different cultures, uncovering their histories, and learning how they shape societies is what ignites my passion. It is a cornerstone of my life's mission to understand, and truly appreciate the wide array of global cultures. Drawing inspiration from each one of them, and merging these experiences into the story that defines me.",
     href: "/mep.jpg",
   },
   {
     id: 3,
     title: "Thank you for visiting!",
-    text: "I hope you've been enjoying my website! Its been a pleasure making it. I strive to make it as user friendly as possible! So if you encounter a problem or have any suggestions for improvements, dont hesitate to contact me. I will gladly answer any questions as good as I can. You will find my contact information below ↓",
+    text: "I hope you've been enjoying my website, Its been a pleasure making it! I will keep making continuos changes. Feel free to contact me if you want to learn more about the techniques used when building this website. You will find my contact information below ↓",
     href: "/mep3.jpg",
   },
 ]
