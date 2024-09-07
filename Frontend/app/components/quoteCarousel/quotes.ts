c46f052662a8ffd6962f66c627f88006236d674b
Jonatan Schultz <jonatan.schultz@blaklader.com>
2024-09-07 20:51:56 +0200

    Quotes

---
+++ Frontend/app/components/quoteCarousel/quotes.ts
@@ -0,0 +1,36 @@
+import { Quote } from "./types/quote";
+
+export const quotes: Quote[] = [
+  {
+    text: "Don't be afraid to start over.. This time, you restart with experience.",
+    author: "Unknown",
+  },
+  {
+    text: "The greatest enemy of knowledge is not ignorance, it is the illusion of knowledge.",
+    author: "Stephen Hawking",
+  },
+  {
+    text: "Questions you cannot answer are usually far better for you than answers you cannot question.",
+    author: "Yuval Noah Harari",
+  },
+  {
+    text: "Our prime purpose in this life is to help others. And if you can't help them, at least don't hurt them.",
+    author: "Dalai Lama",
+  },
+  {
+    text: "Do your little bit of good where you are: it's those little bits of good put together that overwhelm the world.",
+    author: "Desmond Tutu",
+  },
+  {
+    text: "Intelligence is the ability to adapt to change.",
+    author: "Stephen Hawking",
+  },
+  {
+    text: "In our quest for happiness and the avoidance of suffering, we are all fundamentally the same, and therefore equal.",
+    author: "Dalai Lama",
+  },
+  {
+    text: "I am not the sum of my successes and failures. I am the sum of my experiences, and the impact I've had on others.",
+    author: "AI",
+  },
+];
